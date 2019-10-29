c49cb440b82e9df887ece2d68773777265e0a1e6
Shital Shah <shital@ShitalShah.com>
2019-10-29 01:32:07 -0700

    try gym atari install fix

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 pip install --upgrade pip
-pip install ray[rllib]
+
+pip install -q gym
 pip install -q gym[box2d]
 pip install -q gym[classic_control]
 pip install -q gym[atari]
+
+# for some reason we need two tries?
+pip install -q gym
+pip install -q gym[box2d]
+pip install -q gym[classic_control]
+pip install -q gym[atari]
+
+pip install ray[rllib]
+
 cp -f .bashrc ~/.bashrc
 cp -f .bash_aliases ~/.bash_aliases
 echo "set background=dark" >> .vimrc
