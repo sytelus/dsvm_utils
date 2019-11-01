af5d586e917f47da089f5fd4578fb198510ffc32
Shital Shah <shital@ShitalShah.com>
2019-10-31 21:47:24 -0700

    install updates for stable-baselines

--- install.sh
+++ install.sh
@@ -13,6 +13,29 @@ pip install -q gym[atari]
 
 pip install ray[rllib]
 
+sudo apt-get -y update
+sudo apt-get -y install swig cmake libopenmpi-dev zlib1g-dev
+#conda install -y x264=='1!152.20180717' ffmpeg=4.0.2 -c conda-forge
+pip install -q stable-baselines[mpi] box2d box2d-kengz pyyaml pybullet optuna pytablewriter scikit-optimize
+
+mkdir -p ~/GitHubSrc
+pushd ~/GitHubSrc
+function install_from_git {
+	if [ ! -d "~/GitHubSrc/$1" ]; then
+		git clone https://github.com/$1.git
+		cd "$1"
+		pip install -e .
+		cd ..	
+	fi
+}
+
+install_from_git microsoft/tensorwatch
+install_from_git sytelus/podworld
+install_from_git sytelus/regim
+install_from_git hill-a/stable-baselines
+
+popd
+
 cp -f .bashrc ~/.bashrc
 cp -f .bash_aliases ~/.bash_aliases
-echo "set background=dark" >> .vimrc
+#echo "set background=dark" >> .vimrc
--- test_cuda.py
+++ test_cuda.py
@@ -6,10 +6,15 @@ print(l)
 # what devices tensorflow see?
 import tensorflow
 print('TF version', tensorflow.__version__)
-from tensorflow.python.client import device_lib
 
+from tensorflow.python.client import device_lib
 print('Tensorflow devices:')
 print(device_lib.list_local_devices())
+
+import tensorflow as tf
+hello = tf.constant("hello TensorFlow!")
+sess=tf.Session() 
+sess.run(hello
 
 # find PyTorch packages
 import pkg_resources
@@ -23,6 +28,10 @@ print('PyTorch version', torch.__version__)
 print('PyTorch cuda available', cuda.is_available())
 print('PyTorch device count', cuda.device_count())
 print('PyTorch device', cuda.get_device_name(cuda.current_device()))
+
+# confirm Keras sees the GPU
+from keras import backend
+print('keras GPUs:', backend.tensorflow_backend._get_available_gpus())
 
 import os
 os.system('nvidia-smi')
